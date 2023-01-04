7ddf2fa4ef4d65e539f743413a5bf090fb4ef92a
Peter Stan <85736073+Peter-Stan@users.noreply.github.com>
2023-01-04 10:46:02 +0100

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,25 @@
+let display = document.getElementById('display-window');
+
+let buttons = document.getElementsByClassName('button');
+console.log(buttons);
+
+
+
+
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+
+const multiply = function(a, b) {
+    return a * b;
+}
+  
+const divide = function(a,b) {
+    return a / b;
+};
+
